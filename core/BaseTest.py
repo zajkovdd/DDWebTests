4b3c21e15d3e65ffd742a2050b72f66f1c5a7dc7
zajkovdd <zzigqushka@gmail.com>
2025-06-05 10:45:29 +0300

    refactor basepage for cicd

--- core/BaseTest.py
+++ core/BaseTest.py
@@ -5,6 +5,8 @@ from selenium import webdriver
 
 @pytest.fixture(scope="session")
 def browser():
-    driver = webdriver.Chrome()
+    options = webdriver.ChromeOptions()
+    options.add_argument('--lang=ru')
+    driver = webdriver.Remote(command_executor='hhttp://164.215.97.143/:4444', options=options)
     yield driver
     driver.quit()
