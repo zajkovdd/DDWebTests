b7c7c4074e24f198574aca2ec4859329f8f1eb57
zajkovdd <zzigqushka@gmail.com>
2025-06-05 18:17:45 +0300

    refactor BaseTest.py

--- core/BaseTest.py
+++ core/BaseTest.py
@@ -7,6 +7,6 @@ from selenium import webdriver
 def browser():
     options = webdriver.ChromeOptions()
     options.add_argument("--lang=ru")
-    driver = webdriver.Remote(command_executor="http://164.215.97.143:4444/", options=options)
+    driver = webdriver.Remote(command_executor="http://164.215.97.143:4444", options=options)
     yield driver
     driver.quit()
